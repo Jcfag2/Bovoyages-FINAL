6ed1babe9e22dd32850b8760aeffca7c89c00e41
deniskucuk78 <denis.kucuk.pro@gmail.com>
2019-11-30 19:39:25 +0100

    update page voyage details

--- src/app/voyage/components/destination-details/destination-details.component.ts
+++ src/app/voyage/components/destination-details/destination-details.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Destination } from '../../shared/entities/destination';
 import { map } from 'rxjs/operators';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DestinationService } from '../../shared/services/destination.service';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventSource } from '@ng-bootstrap/ng-bootstrap';
+declare var $: any;
 
 @Component({
   selector: 'app-destination-details',
@@ -11,9 +13,11 @@ import { DestinationService } from '../../shared/services/destination.service';
 })
 export class DestinationDetailsComponent implements OnInit {
   @Input() destination:Destination;
-  
+  images = ["monimage.jpg", "lalaa.png"];
+
 
   constructor(private destinationService: DestinationService, private activatedRoute: ActivatedRoute, private router: Router) { }
+  
 
   ngOnInit() {
     this.activatedRoute.paramMap.subscribe(
@@ -21,9 +25,41 @@ export class DestinationDetailsComponent implements OnInit {
         const id = +map.get('id');
         this.destinationService.getDestinationById(id).subscribe(
           destination => this.destination = destination
-        );
-      }
-      )
-      console.log(this.destination);
+        
+          );
+        }
+        )
+        console.log(this.destination);
+
+        $(document).ready(function(){
+          $(".btn-group .btn").click(function(){
+            var inputValue = $(this).find("input").val();
+            if(inputValue != 'all'){
+              var target = $('table tr[data-status="' + inputValue + '"]');
+              $("table tbody tr").not(target).hide();
+              target.fadeIn();
+            } else {
+              $("table tbody tr").fadeIn();
+            }
+          });
+          // Changing the class of status label to support Bootstrap 4
+            var bs = $.fn.tooltip.Constructor.VERSION;
+            var str = bs.split(".");
+            if(str[0] == 4){
+                $(".label").each(function(){
+                  var classStr = $(this).attr("class");
+                    var newClassStr = classStr.replace(/label/g, "badge");
+                    $(this).removeAttr("class").addClass(newClassStr);
+                });
+            }
+        });
     }
+    
+    // images = this.destination.images.map((n) => `http://127.0.0.1:8081/`+n);
+
+  
+    @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
+  
+    
+
 }
--- src/app/voyage/shared/entities/destination.ts
+++ src/app/voyage/shared/entities/destination.ts
@@ -4,16 +4,16 @@ export class Destination {
     id:number;
     region:string;
     descriptif:string;
-    image:string[];
+    images:string[];
     datesVoyages: DatesVoyages;
     deleted:number;
     
 
-    constructor(id:number, region:string, descriptif:string, image:string[], datesVoyages:DatesVoyages, deleted:number){
+    constructor(id:number, region:string, descriptif:string, images:string[], datesVoyages:DatesVoyages, deleted:number){
         this.id = id;
         this.region = region;
         this.descriptif = descriptif;
-        this.image = image;
+        this.images = images;
         this.datesVoyages = datesVoyages;
         this.deleted = deleted;
     }
